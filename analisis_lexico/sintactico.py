92e705549f847e48e01acb314674d303d38aa2e6
cxcarvaj <56209390+cxcarvaj@users.noreply.github.com>
2020-07-10 18:03:00 -0500

    Update sintactico.py | Some sintaxis added

--- analisis_lexico/sintactico.py
+++ analisis_lexico/sintactico.py
@@ -2,25 +2,77 @@ import ply.yacc as sintaxis
 import lexico
 tokens = lexico.tokens
 
-def p_sentencias(p):
-    '''sentencias : asignacion
-    | expresion
-    | metodos '''
 
-def p_imprimir(p):
-    'imprimir : factor'
+def p_sentencias(p):
+    ''' sentencias : asignacion
+    | expresion
+    | metodos
+    | if'''
 
 def p_metodos(p):
     '''metodos : imprimir
-    | toUpper
-    | toLower
-    | startsWith'''
+    | touppercase
+    | tolowercase
+    | startwith
+    | tostring'''
+
+def p_imprimir(p):
+    'imprimir : PRINT LPAREN factor RPAREN'
+
+def p_to_upper_case(p):
+    '''touppercase : ID TOUPPERCASE LPAREN RPAREN
+    | asignacion TOUPPERCASE LPAREN RPAREN '''
+
+
+def p_to_lower_case(p):
+    '''tolowercase : ID TOLOWERCASE LPAREN RPAREN
+    | asignacion TOLOWERCASE LPAREN RPAREN'''
+
+def p_start_with(p):
+    '''startwith : ID STARTSWITH LPAREN STRING RPAREN
+    | asignacion STARTSWITH LPAREN RPAREN '''
+
+def p_to_string(p):
+    '''tostring : ID TOSTRING LPAREN RPAREN
+        | asignacion TOSTRING LPAREN RPAREN '''
+
+def p_if(p):
+    '''if : IF LPAREN condicion RPAREN LBRACE sentencias RBRACE
+    | IF LPAREN condicion RPAREN LBRACE sentencias RBRACE else'''
+
+    # | IF LPAREN condicion RPAREN LBRACE sentencias RBRACE elseif'''
+
+def p_condicion(p):
+    '''condicion : NOT
+    | TRUE
+    | NOT factor
+    | factor EQUALS factor
+    | factor NOTEQUALS factor
+    | factor STRICTEQUALS factor
+    | factor MORETHAN factor
+    | factor LESSTHAN factor
+    | factor MORETHANEQUALS factor
+    | factor LESSTHANEQUALS factor
+    | factor STRICTNOTEQUALS factor '''
+
+def p_else(p):
+    'else : ELSE LBRACE sentencias RBRACE'
+
+# def p_else_if(p):
+#     'elseif : ELSEIF LPAREN condicion RPAREN LBRACE sentencias RBRACE else'
 
 def p_asignacion(p):
-    'asignacion : ID EQUAL expresion'
+    '''asignacion : type ID EQUAL expresion'''
+
+def p_type(p):
+    '''type : VAR
+    | LET '''
 
 def p_expresion(p):
     'expresion : factor'
+
+def p_factor_id(p):
+    'factor : ID'
 
 def p_factor_num(p):
     'factor : NUMBER'
@@ -28,15 +80,20 @@ def p_factor_num(p):
 def p_factor_str(p):
     'factor : STRING'
 
+def p_factor_bool(p):
+    '''factor : TRUE
+    | FALSE '''
+
 
 
 
 
 #Error Generado
 def p_error(p):
-    print("Error de Sintaxis: ")
+    print("No se ha reconocido {}".format(p))
 
-parser = sintaxis.yacc()
+
+parser=sintaxis.yacc()
 
 while True:
     try:
